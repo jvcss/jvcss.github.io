25143498721f85d3339a0508194f1646969ce3e3
jvcss <jvcs.mecatronica@gmail.com>
2023-12-02 14:31:27 -0300

    add post creation

---
+++ _posts/2023-12-02-flutter-swiping-cards.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title: "FlutterSwipingCards"
+date: 2023-12-02 14:28:45 
+categories: flutter animation card swiping
+---
+
+Let's create an Flutter app to make animations with cards
---
+++ posting.py
@@ -0,0 +1,42 @@
+import os
+import sys
+from datetime import datetime
+
+def create_new_post(title):
+    # Set the path to the _posts directory
+    posts_directory = "_posts"
+
+    # Convert title to camel case and remove hyphens
+    camel_case_title = ''.join(word.capitalize() for word in title.split('-'))
+
+    # Generate the filename based on the current date and post title
+    date_prefix = datetime.now().strftime("%Y-%m-%d")
+    filename = f"{date_prefix}-{title.lower().replace(' ', '-')}.md"
+
+    # Set the full path to the new post file
+    post_path = os.path.join(posts_directory, filename)
+
+    # Create the new post file with front matter
+    with open(post_path, "w") as file:
+        file.write(f"---\n")
+        file.write(f"layout: post\n")
+        file.write(f"title: \"{camel_case_title}\"\n")
+        file.write(f"date: {datetime.now().strftime('%Y-%m-%d %H:%M:%S %z')}\n")
+        file.write(f"categories: []\n")  # Add categories as needed
+        file.write(f"---\n\n")
+        file.write(f"New Post! .\n")
+
+    print(f"New post created: {post_path}")
+
+    # Open the new post file in the default text editor
+    try:
+        os.system(f"code {os.path.abspath(post_path)}")
+    except Exception as e:
+        print(f"Error opening the text editor: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python create_post.py 'Your Post Title'")
+    else:
+        post_title = sys.argv[1]
+        create_new_post(post_title)
